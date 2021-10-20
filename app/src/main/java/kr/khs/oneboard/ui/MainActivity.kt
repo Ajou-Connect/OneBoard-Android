695c33559c1cb79b2d17580ce0824fa231b31bc9
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-20 21:52:05 +0900

    [3.01] Add `DrawerLayout`, `NavigationView`, `ActionBarDrawerToggle` to show `Profile` in `Menu`.

--- app/src/main/java/kr/khs/oneboard/ui/MainActivity.kt
+++ app/src/main/java/kr/khs/oneboard/ui/MainActivity.kt
@@ -1,14 +1,73 @@
 package kr.khs.oneboard.ui
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import dagger.hilt.android.AndroidEntryPoint
 import kr.khs.oneboard.R
+import kr.khs.oneboard.databinding.ActivityMainBinding
+import kr.khs.oneboard.databinding.DrawerHeaderBinding
+import kr.khs.oneboard.utils.UserInfoUtil
+import kr.khs.oneboard.viewmodels.MainViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val viewModel: MainViewModel by viewModels()
+
+    private lateinit var drawerHeaderBinding: DrawerHeaderBinding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        initDrawer()
+        initNavigationView()
+    }
+
+    private fun initDrawer() {
+        actionBarDrawerToggle =
+            ActionBarDrawerToggle(this, binding.drawerLayout, R.string.app_name, R.string.app_name)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBarDrawerToggle.syncState()
+
+        binding.drawerLayout.addDrawerListener(actionBarDrawerToggle)
+
+        drawerHeaderBinding = DrawerHeaderBinding.bind(binding.navigationView.getHeaderView(0))
+
+        initDrawerHeader()
+    }
+
+    private fun initDrawerHeader() {
+        with(drawerHeaderBinding) {
+            drawerEmail.text = UserInfoUtil.email
+            drawerMajor.text = UserInfoUtil.major
+            drawerName.text = UserInfoUtil.name
+        }
+    }
+
+    private fun initNavigationView() {
+        // todo 교수, 학생일 경우 다른 메뉴 inflate
+        binding.navigationView.inflateMenu(R.menu.drawer_menu_professor)
+
+        binding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+
+            }
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+            false
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        actionBarDrawerToggle.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item)
     }
 }
