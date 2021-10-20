9c7d0dde035d63e95f367f566dd52e35dc4063a8
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-20 16:27:02 +0900

    Add `SplashActivity Animation`

--- app/src/main/java/kr/khs/oneboard/ui/SplashActivity.kt
+++ app/src/main/java/kr/khs/oneboard/ui/SplashActivity.kt
@@ -4,10 +4,18 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.*
+import kr.khs.oneboard.R
+import kr.khs.oneboard.api.ApiService
 import kr.khs.oneboard.databinding.ActivitySplashBinding
 import kr.khs.oneboard.utils.DialogUtil
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SplashActivity : AppCompatActivity() {
     private val binding: ActivitySplashBinding by lazy {
         ActivitySplashBinding.inflate(
@@ -15,28 +23,66 @@ class SplashActivity : AppCompatActivity() {
         )
     }
 
+    @Inject
+    lateinit var apiService: ApiService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        networkCheck()
-        loginCheck()
+        val text1Anim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_textview)
+        binding.splashTitle1.startAnimation(text1Anim)
+        val text2Anim = AnimationUtils.loadAnimation(this, R.anim.anim_splash_textview)
+        binding.splashTitle2.startAnimation(text2Anim)
 
-        // todo 필요할 경우 미리 데이터 로딩
-        Runnable {
-            Thread.sleep(2000L)
-            startActivity(
-                Intent(applicationContext, MainActivity::class.java)
-            )
-        }.run()
+        text2Anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                networkCheck()
+                serverCheck()
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+            }
+
+        })
     }
 
-    private fun loginCheck() {
-        // todo 로그인 체크
-        if (false) {
-            startActivity(
-                Intent(applicationContext, LoginActivity::class.java)
-            )
+    // health 체크 후 로그인 체크
+    private fun serverCheck() {
+        runBlocking {
+            val healthCheck: Deferred<Boolean> = async(Dispatchers.IO) {
+//                apiService.healthCheck()
+                // TODO: 2021/10/19 test data
+                true
+            }
+
+            if (healthCheck.await()) {
+                startActivity(
+                    Intent(
+                        applicationContext,
+//                    if (apiService.loginCheck())
+//                        MainActivity::class.java
+//                    else
+                        LoginActivity::class.java
+                    )
+                )
+                overridePendingTransition(
+                    R.anim.anim_activity_in_down,
+                    R.anim.anim_activity_out_top
+                )
+            } else {
+                DialogUtil.createDialog(
+                    context = this@SplashActivity,
+                    message = "서버의 상태가 불안정합니다.",
+                    positiveText = "앱 재시작",
+                    negativeText = "앱 종료",
+                    positiveAction = { restart() },
+                    negativeAction = { killMyApp() }
+                )
+            }
         }
     }
 
@@ -51,10 +97,7 @@ class SplashActivity : AppCompatActivity() {
                 positiveText = "앱 재시작",
                 negativeText = "앱 종료",
                 positiveAction = { restart() },
-                negativeAction = {
-                    val pid = android.os.Process.myPid()
-                    android.os.Process.killProcess(pid)
-                }
+                negativeAction = { killMyApp() }
             )
         }
     }
@@ -67,4 +110,9 @@ class SplashActivity : AppCompatActivity() {
         startActivity(mainIntent)
         Runtime.getRuntime().exit(0)
     }
+
+    private fun killMyApp() = android.os.Process.killProcess(
+        android.os.Process.myPid()
+    )
+
 }
