9b35be5f6595227d6d2dd9495ec5ac9cbe353f9f
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-26 19:23:52 +0900

    Add `Loading Animation`

--- app/src/main/java/kr/khs/oneboard/utils/DialogUtil.kt
+++ app/src/main/java/kr/khs/oneboard/utils/DialogUtil.kt
@@ -1,9 +1,18 @@
 package kr.khs.oneboard.utils
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.ColorDrawable
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import kr.khs.oneboard.R
 
 object DialogUtil {
+    private var loadingDialog: AppCompatDialog? = null
+
     fun createDialog(
         context: Context,
         message: String,
@@ -22,5 +31,35 @@ object DialogUtil {
                 dialog.dismiss()
             }
             .show()
+    }
+
+    fun onLoadingDialog(activity: Activity?) {
+        if (activity == null || activity.isFinishing)
+            return
+
+        loadingDialog?.let {
+            if (it.isShowing)
+                return
+        }
+
+        loadingDialog = AppCompatDialog(activity).apply {
+            setCancelable(false)
+            window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            setContentView(R.layout.dialog_loading)
+        }
+
+        loadingDialog?.show()
+
+        val loadingImage = loadingDialog?.findViewById<ImageView>(R.id.loadingImage)
+        val loadingAni = loadingImage!!.background as AnimationDrawable
+        loadingImage.post { loadingAni.start() }
+    }
+
+    fun offLoadingDialog() {
+        loadingDialog?.let {
+            if (it.isShowing) {
+                loadingDialog?.dismiss()
+            }
+        }
     }
 }
