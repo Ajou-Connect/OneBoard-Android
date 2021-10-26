9c3a8eba6ac34dd94ef84cdf9cf1918127fe0ed3
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-26 20:16:15 +0900

    Add `Parcelize` in `Lecture` Data class.

--- app/src/main/java/kr/khs/oneboard/data/Lecture.kt
+++ app/src/main/java/kr/khs/oneboard/data/Lecture.kt
@@ -1,8 +1,12 @@
 package kr.khs.oneboard.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Lecture(
     val id: Int,
     val title: String,
     val semester: String,
     val professor: String
-)
+) : Parcelable
