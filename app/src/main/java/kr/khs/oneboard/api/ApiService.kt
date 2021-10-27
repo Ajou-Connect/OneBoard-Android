3083aafb0251e82fab3f2e6765dd7cbe2bb7ae9c
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-27 21:28:42 +0900

    Add Api `lecture/notice` ~ `lecture/assignment`

--- app/src/main/java/kr/khs/oneboard/api/ApiService.kt
+++ app/src/main/java/kr/khs/oneboard/api/ApiService.kt
@@ -1,6 +1,8 @@
 package kr.khs.oneboard.api
 
+import kr.khs.oneboard.data.Assignment
 import kr.khs.oneboard.data.Lecture
+import kr.khs.oneboard.data.Notice
 import kr.khs.oneboard.data.api.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -18,11 +20,34 @@ interface ApiService {
     @POST("auth/check")
     suspend fun loginCheck(): Response<Any>
 
-    @GET("/user")
+    @GET("user")
     suspend fun getUserInfo(): Response<Any>
 
-    @GET("/user/lectures")
+    @GET("user/lectures")
     suspend fun getUserLectures(): Response<List<Lecture>>
 
-    // todo /lectures 부터
+    @GET("lecture")
+    suspend fun getUserLecture(): Response<Lecture>
+
+    @GET("lecture/notice")
+    suspend fun getNoticeList(lectureId: Int): Response<List<Notice>>
+
+    // todo 등록, 수정, 삭제
+    @POST("lecture/notice")
+    suspend fun postNotice()
+
+//    @GET("lecture/attendance")
+//    suspend fun getAttendance()
+//
+//    @POST("lecture/attendance")
+//    suspend fun postAttendance()
+
+    @GET("lecture/assignment")
+    suspend fun getAssignmentList(lectureId: Int): Response<List<Assignment>>
+
+    // todo 등록, 수정, 삭제
+    @POST("lecture/assignment")
+    suspend fun postAssignment()
+
+    // todo lecture/assignment/result부터
 }
