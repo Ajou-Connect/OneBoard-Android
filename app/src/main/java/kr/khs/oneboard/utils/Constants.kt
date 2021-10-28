331900a6d483d290dfe83f6092e087238d096aa3
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-28 13:27:27 +0900

    Add `TYPE` constants which is `Boolean`

--- app/src/main/java/kr/khs/oneboard/utils/Constants.kt
+++ app/src/main/java/kr/khs/oneboard/utils/Constants.kt
@@ -4,3 +4,9 @@ const val API_URL = "http://115.85.182.194:8080/"
 
 const val SUCCESS = "SUCCESS"
 const val FAIL = "FAIL"
+
+const val TYPE_PROFESSOR = true
+const val TYPE_STUDENT = false
+
+const val TYPE_NOTICE = true
+const val TYPE_ASSIGNMENT = false
