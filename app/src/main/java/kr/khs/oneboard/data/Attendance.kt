d68f512362824576417c722002181e05525cf97b
KHS-kr <ks96ks@ajou.ac.kr>
2021-10-29 00:52:16 +0900

    Add `AttendanceStudent`, `AttendanceLesson` data class to manage attendance data

---
+++ app/src/main/java/kr/khs/oneboard/data/Attendance.kt
@@ -0,0 +1,29 @@
+package kr.khs.oneboard.data
+
+import com.squareup.moshi.Json
+
+// list가 1이면 바로 확장되도록
+data class AttendanceStudent(
+    @Json(name = "lesson_id")
+    val lessonId: Int,
+    @Json(name = "student_id")
+    val studentId: Int,
+    @Json(name = "student_major")
+    val studentMajor: String,
+    @Json(name = "student_name")
+    val studentName: String,
+    val lessonList: List<AttendanceLesson>,
+    var isExpand: Boolean
+)
+
+data class AttendanceLesson(
+    val id: Int,
+    val date: String,
+    val description: String,
+    var check: Boolean
+)
+
+// A 출석, B 지각, F 결석
+enum class AttendanceEnum {
+    A, B, F
+}
