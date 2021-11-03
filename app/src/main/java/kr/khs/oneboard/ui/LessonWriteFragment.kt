141af2af376df5b2a464bda3c84d2dcd65c0ebce
KHS-kr <ks96ks@ajou.ac.kr>
2021-11-03 17:50:20 +0900

    Add `DatePickerDialog`, `TimePickerDialog`, set `Date`, `Time` Listener

--- app/src/main/java/kr/khs/oneboard/ui/LessonWriteFragment.kt
+++ app/src/main/java/kr/khs/oneboard/ui/LessonWriteFragment.kt
@@ -1,5 +1,7 @@
 package kr.khs.oneboard.ui
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,10 +18,36 @@ import kr.khs.oneboard.utils.TYPE_NON_FACE_TO_FACE
 import kr.khs.oneboard.utils.TYPE_RECORDING
 import kr.khs.oneboard.utils.ToastUtil
 import kr.khs.oneboard.viewmodels.LessonWriteViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.properties.Delegates
 
 @AndroidEntryPoint
 class LessonWriteFragment : BaseFragment<FragmentLessonWriteBinding, LessonWriteViewModel>() {
     override val viewModel: LessonWriteViewModel by viewModels()
+    private var year by Delegates.notNull<Int>()
+    private var month by Delegates.notNull<Int>()
+    private var day by Delegates.notNull<Int>()
+    private var hour by Delegates.notNull<Int>()
+    private var minute by Delegates.notNull<Int>()
+
+    private val onDateSetListener by lazy {
+        DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+            this.year = year
+            this.month = month
+            this.day = dayOfMonth
+            binding.lessonWriteDate.text =
+                String.format("%04d-%02d-%02d", year, month + 1, dayOfMonth)
+        }
+    }
+
+    private val onTimeSetListener by lazy {
+        TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+            this.hour = hourOfDay
+            this.minute = minute
+            binding.lessonWriteTime.text = String.format("%02d:%02d", hour, minute)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -62,9 +90,82 @@ class LessonWriteFragment : BaseFragment<FragmentLessonWriteBinding, LessonWrite
 
     override fun init() {
         initTitle()
+        initTimeDate()
         initWriteLessonButton()
         initLessonTypeSpinner()
         initShowButton()
+    }
+
+    private fun initTimeDate() {
+        val c = Calendar.getInstance()
+        year = c.get(Calendar.YEAR)
+        month = c.get(Calendar.MONTH)
+        day = c.get(Calendar.DAY_OF_MONTH)
+
+        val time = SimpleDateFormat("hh:mm").format(c.time).split(":")
+        hour = time[0].toInt()
+        minute = time[1].toInt()
+
+        val minDate = Calendar.getInstance().apply {
+            set(year, month, day)
+        }
+
+        // 1안 : 이전 수업과 동일 체크박스
+        // 이전 수업과 동일로 할 경우 날짜는 어떻게?
+        binding.lessonWriteCheckbox.text = "이전 수업과 동일"
+        binding.lessonWriteCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                binding.lessonWriteDate.text = "2021-11-04"
+                binding.lessonWriteTime.text = "16:30"
+                setDateTimeEnabled(false)
+            } else {
+                setDateTimeEnabled(true)
+            }
+        }
+        binding.lessonWriteDate.text = "2021-11-04"
+        binding.lessonWriteTime.text = "16:30"
+
+        // 2안 : 가장 가까운 날짜 디폴트로, 체크박스를 통해 수정 가능
+        binding.lessonWriteCheckbox.text = "수정하기"
+        setDateTimeEnabled(false)
+        binding.lessonWriteCheckbox.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                setDateTimeEnabled(true)
+            } else {
+                binding.lessonWriteDate.text = "2021-11-04"
+                binding.lessonWriteTime.text = "16:30"
+                setDateTimeEnabled(false)
+            }
+        }
+        binding.lessonWriteDate.text = "2021-11-04"
+        binding.lessonWriteTime.text = "16:30"
+
+        // 1, 2안 끝
+        binding.lessonWriteDate.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                onDateSetListener,
+                year,
+                month,
+                day
+            ).apply {
+                datePicker.minDate = minDate.time.time
+            }.show()
+        }
+        binding.lessonWriteTime.setOnClickListener {
+            TimePickerDialog(
+                requireContext(),
+                onTimeSetListener,
+                hour,
+                minute,
+                true
+            ).show()
+        }
+    }
+
+    private fun setDateTimeEnabled(enabled: Boolean) {
+        binding.lessonWriteDate.isEnabled = enabled
+        binding.lessonWriteTime.isEnabled = enabled
     }
 
     private fun initTitle() {
