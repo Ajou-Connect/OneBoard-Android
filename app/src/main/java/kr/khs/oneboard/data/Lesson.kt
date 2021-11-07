91bc8c2edfd1b680b78ee2706a22674c32fc0744
KHS-kr <ks96ks@ajou.ac.kr>
2021-11-07 17:27:51 +0900

    Add Parcelable in `Lesson` data class

--- app/src/main/java/kr/khs/oneboard/data/Lesson.kt
+++ app/src/main/java/kr/khs/oneboard/data/Lesson.kt
@@ -1,5 +1,9 @@
 package kr.khs.oneboard.data
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Lesson(
     val id: Int,
     val title: String,
@@ -9,4 +13,4 @@ data class Lesson(
     val room: String? = null,
     val meetingId: String? = null,
     val videoUrl: String? = null
-)
+) : Parcelable
