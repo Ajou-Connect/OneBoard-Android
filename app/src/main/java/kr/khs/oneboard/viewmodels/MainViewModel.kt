9a3df6aede46c792042322edc6008b5225c1ba2a
KHS-kr <ks96ks@ajou.ac.kr>
2021-11-20 14:39:24 +0900

    Add get `DefaultDayTime` function that `calculate` and `return` default value

--- app/src/main/java/kr/khs/oneboard/viewmodels/MainViewModel.kt
+++ app/src/main/java/kr/khs/oneboard/viewmodels/MainViewModel.kt
@@ -10,6 +10,10 @@ import kr.khs.oneboard.core.UseCase
 import kr.khs.oneboard.data.Lecture
 import kr.khs.oneboard.data.User
 import kr.khs.oneboard.repository.UserRepository
+import kr.khs.oneboard.utils.getDay
+import kr.khs.oneboard.utils.toDayOfWeekInt
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -26,6 +30,31 @@ class MainViewModel @Inject constructor(private val userRepository: UserReposito
     }
 
     fun getLecture() = lecture.value!!
+
+    fun getLectureDayTime(): Pair<String, String>? {
+        lecture.value ?: return null
+
+        val lectureDateTime = lecture.value?.defaultDateTime?.split(",") ?: return null
+
+        loop@ for (dateTime in lectureDateTime) {
+            return try {
+                val split = dateTime.split(" ")
+                if (split[0].toDayOfWeekInt() < getDay().toDayOfWeekInt())
+                    continue@loop
+                else {
+                    val date = SimpleDateFormat("MM-dd").format(
+                        Calendar.getInstance().timeInMillis + 1000 * 60 * 60 * 24L * (getDay().toDayOfWeekInt() - split[0].toDayOfWeekInt())
+                    )
+                    Pair(date, split[1])
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                null
+            }
+        }
+
+        return null
+    }
 
     fun getUserInfo() {
         viewModelScope.launch {
