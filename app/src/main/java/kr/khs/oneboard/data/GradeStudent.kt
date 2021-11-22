51455d886a0db9dece513100a8a9f12a19dfd9a9
KHS-kr <ks96ks@ajou.ac.kr>
2021-11-22 20:48:10 +0900

    Add `GradeStudent`, `GradeSubmit`, `GradeRatio` data class

--- app/src/main/java/kr/khs/oneboard/data/GradeStudent.kt
+++ app/src/main/java/kr/khs/oneboard/data/GradeStudent.kt
@@ -3,13 +3,28 @@ package kr.khs.oneboard.data
 import com.squareup.moshi.Json
 
 data class GradeStudent(
-    @Json(name = "lecture_id")
     val lectureId: Int,
-    @Json(name = "student_id")
-    val studentId: String,
-    @Json(name = "student_name")
-    val studentName: String,
-    val studentMajor: String,
-    val assignmentList: List<Pair<String, Int>>,
-    val score: Float?
+    val lectureTitle: String,
+    val userId: Int,
+    val userName: String,
+    val studentNumber: String,
+    val totalScore: Float,
+    val submitScore: Float,
+    val attendScore: Float,
+    val result: String,
+    val submitList: List<GradeSubmit>? = null,
+) {
+    data class GradeSubmit(
+        val submitId: Int,
+        val assignmentId: Int,
+        val assignmentTitle: String,
+        val score: Float
+    )
+}
+
+data class GradeRatio(
+    @Json(name = "aratio")
+    val aRatio: Int,
+    @Json(name = "bratio")
+    val bRatio: Int
 )
