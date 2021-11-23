5606404c97ad6fbbe50ef1c09f90ca85f29a6d7f
KHS-kr <ks96ks@ajou.ac.kr>
2021-11-23 21:58:42 +0900

    Add `Uri.getFileName` function to get `Real File Name`

--- app/src/main/java/kr/khs/oneboard/utils/FileUtil.kt
+++ app/src/main/java/kr/khs/oneboard/utils/FileUtil.kt
@@ -1,19 +1,25 @@
 package kr.khs.oneboard.utils
 
+import android.annotation.SuppressLint
 import android.content.ContentResolver
+import android.database.Cursor
 import android.net.Uri
+import android.provider.OpenableColumns
 import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import okio.BufferedSink
 import okio.source
+import timber.log.Timber
+
 
 // https://ohdbjj.tistory.com/49
 fun Uri.asMultipart(name: String, contentResolver: ContentResolver): MultipartBody.Part? {
     return contentResolver.query(this, null, null, null, null)?.let {
         if (it.moveToNext()) {
-            val displayName = this.lastPathSegment
+            Timber.tag("ricky").d(this.getFileName(contentResolver))
+            val displayName = this.getFileName(contentResolver)
             val requestBody = object : RequestBody() {
                 override fun contentType(): MediaType? {
                     return contentResolver.getType(this@asMultipart)?.toMediaType()
@@ -30,4 +36,23 @@ fun Uri.asMultipart(name: String, contentResolver: ContentResolver): MultipartBo
             null
         }
     }
+}
+
+@SuppressLint("Range")
+fun Uri.getFileName(contentResolver: ContentResolver): String {
+    var result: String? = null
+    if (this.scheme == "content") {
+        val cursor: Cursor? = contentResolver.query(this, null, null, null, null)
+        try {
+            if (cursor != null && cursor.moveToFirst()) {
+                result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+            }
+        } finally {
+            cursor?.close()
+        }
+    }
+    if (result == null) {
+        result = this.lastPathSegment
+    }
+    return result ?: "nullll"
 }
