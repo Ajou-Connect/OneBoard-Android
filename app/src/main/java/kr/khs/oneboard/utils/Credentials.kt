a88933918385f20c6735ff4d3d73c7782427482b
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-02 20:23:09 +0900

    Add `Credentials.kt`, Remove it in `.gitignore` to `CI/CD`

---
+++ app/src/main/java/kr/khs/oneboard/utils/Credentials.kt
@@ -0,0 +1,6 @@
+package kr.khs.oneboard.utils
+
+const val WEB_DOMAIN = "https://zoom.us"
+const val token = ""
+const val sdkKey = "MoRylmD2jBq9NfbZXbSVmvZcGYOFkDCeJc3e"
+const val sdkSecret = "NewabYwGXIFrOlPRf4dZBKeqFECESIkdlLrq"
