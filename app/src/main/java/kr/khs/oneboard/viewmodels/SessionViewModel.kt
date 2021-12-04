e75b9db8bf07e87e6d07d31b5778e3b70197b407
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-04 12:38:56 +0900

    Set skeleton code in `SessionViewModel`

--- app/src/main/java/kr/khs/oneboard/viewmodels/SessionViewModel.kt
+++ app/src/main/java/kr/khs/oneboard/viewmodels/SessionViewModel.kt
@@ -20,11 +20,15 @@ class SessionViewModel @Inject constructor(private val repository: SessionReposi
     private var lectureId by Delegates.notNull<Int>()
     private var lessonId by Delegates.notNull<Int>()
 
+    // TODO: 2021/12/04 Set live ID
+    private var liveId = 0
+
     private val _isLeave = MutableLiveData(false)
     val isLeave: LiveData<Boolean> = _isLeave
 
     fun postAttendance() {
         viewModelScope.launch {
+            showProgress()
             val response =
                 if (UserInfoUtil.type == TYPE_PROFESSOR)
                     repository.postAttendanceProfessor(lectureId, lessonId)
@@ -41,23 +45,80 @@ class SessionViewModel @Inject constructor(private val repository: SessionReposi
             } else {
                 setErrorMessage("오류가 발생했습니다.")
             }
+            hideProgress()
         }
     }
 
-    fun getUnderStanding() {
+    fun getUnderStanding(understandingId: Int) {
+        viewModelScope.launch {
+            showProgress()
+            val response = repository.getUnderStanding(lectureId, lessonId, liveId, understandingId)
 
+            if (response.status == UseCase.Status.SUCCESS) {
+                val data = response.data!!
+
+                // TODO: 2021/12/04 데이터 처리
+            } else {
+                setErrorMessage("이해도 평가 정보를 가져오지 못했습니다.")
+            }
+            hideProgress()
+        }
     }
 
     fun postUnderStanding() {
+        viewModelScope.launch {
+            showProgress()
+            val response = repository.postUnderStanding(lectureId, lessonId, liveId)
 
+            if (response.status == UseCase.Status.SUCCESS) {
+                setErrorMessage(
+                    if (response.data!!) {
+                        "전송 완료"
+                    } else {
+                        "전송 실패"
+                    }
+                )
+            } else {
+                setErrorMessage("이해도 평가 중 오류가 발생했습니다.")
+            }
+            hideProgress()
+        }
     }
 
     fun getQuiz() {
+        viewModelScope.launch {
+            showProgress()
+            val response = repository.getQuiz(lectureId, lessonId, liveId)
 
+            if (response.status == UseCase.Status.SUCCESS) {
+                val data = response.data!!
+
+                // TODO: 2021/12/04 데이터 처리
+            } else {
+                setErrorMessage("이해도 평가 정보를 가져오지 못했습니다.")
+            }
+            hideProgress()
+        }
     }
 
-    fun postQuiz() {
+    fun postQuiz(quizId: Int) {
+        viewModelScope.launch {
+            showProgress()
+            val response = repository.postQuiz(lectureId, lessonId, liveId, quizId)
 
+            if (response.status == UseCase.Status.SUCCESS) {
+                setErrorMessage(
+                    if (response.data!!) {
+                        "전송 완료"
+                    } else {
+                        "전송 실패"
+                    }
+                )
+            } else {
+                setErrorMessage("퀴즈 정보를 전송하지 못했습니다.")
+            }
+            hideProgress()
+        }
     }
 
     fun leaveSession() {
@@ -79,3 +140,5 @@ class SessionViewModel @Inject constructor(private val repository: SessionReposi
         this.lessonId = lessonId
     }
 }
+
+// TODO: 2021/12/03 테스트하기 : 줌 세션 나가기(강의자, 학생) 
