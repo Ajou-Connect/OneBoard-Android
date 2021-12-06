7f46206b53f6e228de16af6199b18c229a67808f
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-06 20:37:59 +0900

    Add function for fix `Attendance` for three types.s

--- app/src/main/java/kr/khs/oneboard/viewmodels/AttendanceViewModel.kt
+++ app/src/main/java/kr/khs/oneboard/viewmodels/AttendanceViewModel.kt
@@ -8,24 +8,33 @@ import kotlinx.coroutines.launch
 import kr.khs.oneboard.core.BaseViewModel
 import kr.khs.oneboard.core.UseCase
 import kr.khs.oneboard.data.AttendanceStudent
+import kr.khs.oneboard.data.Lesson
+import kr.khs.oneboard.data.request.AttendanceUpdateRequestDto
 import kr.khs.oneboard.repository.LectureRepository
+import kr.khs.oneboard.repository.LessonRepository
 import kr.khs.oneboard.utils.toUpdateRequestDTO
 import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class AttendanceViewModel @Inject constructor(private val lectureRepository: LectureRepository) :
+class AttendanceViewModel @Inject constructor(
+    private val lectureRepository: LectureRepository,
+    private val lessonRepository: LessonRepository
+) :
     BaseViewModel() {
 
     private val _attendanceList = MutableLiveData<List<AttendanceStudent>>()
     val attendanceList: LiveData<List<AttendanceStudent>>
         get() = _attendanceList
 
+    val lessonList = MutableLiveData<List<Lesson>>()
+
     fun getAttendanceList(lectureId: Int) {
         viewModelScope.launch {
             showProgress()
             val response = lectureRepository.getAttendanceList(lectureId)
             if (response.status == UseCase.Status.SUCCESS) {
+                lessonList.value = lessonRepository.getLessonList(lectureId).data!!
                 _attendanceList.value = response.data!!
             } else {
                 setErrorMessage("출석 목록을 불러오지 못했습니다.")
@@ -84,6 +93,65 @@ class AttendanceViewModel @Inject constructor(private val lectureRepository: Lec
                 setErrorMessage("출석 목록을 불러오지 못했습니다.")
             }
             hideProgress()
+        }
+    }
+
+    // isAttend - true : 2 attendance, false : 0 absent
+    // if lessonId = -1 -> target : All students or specific student, else target : specific lesson
+    // if studentId = -1 -> target : All students or specific lesson, else target : specific student
+    fun changeAllAttendance(
+        lectureId: Int,
+        isAttend: Boolean,
+        lessonIdx: Int = -1,
+        studentIdx: Int = -1
+    ) {
+
+        viewModelScope.launch {
+            showProgress()
+            val requestDto =
+                when {
+                    lessonIdx != -1 -> {
+                        val lessonId = lessonList.value!![lessonIdx].lessonId
+                        AttendanceUpdateRequestDto(
+                            attendanceList.value!!.map {
+                                AttendanceUpdateRequestDto.AttendanceUpdate(
+                                    it.studentId,
+                                    lessonId,
+                                    if (isAttend) 2 else 0
+                                )
+                            }
+                        )
+                    }
+                    studentIdx != -1 -> {
+                        val studentId = attendanceList.value!![studentIdx].studentId
+                        AttendanceUpdateRequestDto(
+                            attendanceList.value!!.filter { it.studentId == studentId }[0].attendanceList.map {
+                                AttendanceUpdateRequestDto.AttendanceUpdate(
+                                    studentId,
+                                    it.lessonId,
+                                    if (isAttend) 2 else 0
+                                )
+                            }
+                        )
+                    }
+                    else -> {
+                        AttendanceUpdateRequestDto(attendanceList.value!!.toUpdateRequestDTO().updateDataList.map {
+                            it.apply { it.status = if (isAttend) 2 else 0 }
+                        })
+                    }
+                }
+
+
+            val response = lectureRepository.postAttendanceList(lectureId, requestDto)
+
+            if (response.status == UseCase.Status.SUCCESS && response.data!!) {
+                setErrorMessage("저장되었습니다.")
+            } else {
+                setErrorMessage("출석 현황 업데이트를 실패했습니다.\n다시 시도해주세요.")
+            }
+
+            hideProgress()
+            getAttendanceList(lectureId)
         }
     }
 }
