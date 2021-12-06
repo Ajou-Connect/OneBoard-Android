f6a432f64f8380d8488a4431ec6539e2270043d0
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-06 21:24:23 +0900

    Fix to parse date without seconds

--- app/src/main/java/kr/khs/oneboard/ui/LectureWriteFragment.kt
+++ app/src/main/java/kr/khs/oneboard/ui/LectureWriteFragment.kt
@@ -21,7 +21,9 @@ import kr.khs.oneboard.data.request.AssignmentUpdateRequestDto
 import kr.khs.oneboard.data.request.NoticeUpdateRequestDto
 import kr.khs.oneboard.databinding.FragmentLectureWriteBinding
 import kr.khs.oneboard.extensions.toDateTime
+import kr.khs.oneboard.extensions.toDateTimeWithoutSec
 import kr.khs.oneboard.extensions.toTimeInMillis
+import kr.khs.oneboard.extensions.toTimeInMillisWithoutSec
 import kr.khs.oneboard.utils.*
 import kr.khs.oneboard.viewmodels.LectureWriteViewModel
 import okhttp3.MultipartBody
@@ -204,7 +206,7 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                         AM_PM: String
                     ) {
                         (textview as TextView).text = String.format(
-                            "%04d-%02d-%02d %02d:%02d:00",
+                            "%04d-%02d-%02d %02d:%02d",
                             year,
                             monthNumber + 1,
                             day,
@@ -261,8 +263,8 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                         null,
                     if (type == TYPE_ASSIGNMENT) {
                         if (binding.writeStartDt.text.toString()
-                                .toTimeInMillis() >= binding.writeEndDt.text.toString()
-                                .toTimeInMillis()
+                                .toTimeInMillisWithoutSec() >= binding.writeEndDt.text.toString()
+                                .toTimeInMillisWithoutSec()
                         ) {
                             viewModel.setErrorMessage("시작 시간이 마감 시간보다 크거나 같을 수 없습니다.")
                             return@setOnClickListener
@@ -274,8 +276,8 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                             binding.writeTitleEditText.text.toString(),
                             binding.writeContentEditText.html,
                             "",
-                            binding.writeStartDt.text.toString(),
-                            binding.writeEndDt.text.toString(),
+                            binding.writeStartDt.text.toString() + ":00",
+                            binding.writeEndDt.text.toString() + ":00",
                             if (binding.writeExposeTimeCheckBox.isChecked)
                                 System.currentTimeMillis().toDateTime()
                             else
@@ -303,8 +305,8 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                         null,
                     if (type == TYPE_ASSIGNMENT) {
                         if (binding.writeStartDt.text.toString()
-                                .toTimeInMillis() >= binding.writeEndDt.text.toString()
-                                .toTimeInMillis()
+                                .toTimeInMillisWithoutSec() >= binding.writeEndDt.text.toString()
+                                .toTimeInMillisWithoutSec()
                         ) {
                             viewModel.setErrorMessage("시작 시간이 마감 시간보다 크거나 같을 수 없습니다.")
                             return@setOnClickListener
@@ -316,8 +318,8 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                             binding.writeTitleEditText.text.toString(),
                             binding.writeContentEditText.html,
                             "",
-                            binding.writeStartDt.text.toString(),
-                            binding.writeEndDt.text.toString(),
+                            binding.writeStartDt.text.toString() + ":00",
+                            binding.writeEndDt.text.toString() + ":00",
                             if (binding.writeExposeTimeCheckBox.isChecked)
                                 System.currentTimeMillis().toDateTime()
                             else
@@ -355,8 +357,8 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
             }
             TYPE_ASSIGNMENT -> {
                 binding.writeStartEndDT.visibility = View.VISIBLE
-                binding.writeStartDt.text = System.currentTimeMillis().toDateTime()
-                binding.writeEndDt.text = System.currentTimeMillis().toDateTime()
+                binding.writeStartDt.text = System.currentTimeMillis().toDateTimeWithoutSec()
+                binding.writeEndDt.text = System.currentTimeMillis().toDateTimeWithoutSec()
 
                 binding.writeStartDt.setOnClickListener { textview ->
                     CustomDateTimePicker(
@@ -382,13 +384,12 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                                 AM_PM: String
                             ) {
                                 (textview as TextView).text = String.format(
-                                    "%04d-%02d-%02d %02d:%02d:%02d",
+                                    "%04d-%02d-%02d %02d:%02d",
                                     year,
                                     monthNumber + 1,
                                     day,
                                     hour24,
-                                    min,
-                                    sec
+                                    min
                                 )
                             }
                         }).apply {
@@ -423,13 +424,12 @@ class LectureWriteFragment : BaseFragment<FragmentLectureWriteBinding, LectureWr
                                 AM_PM: String
                             ) {
                                 (textview as TextView).text = String.format(
-                                    "%04d-%02d-%02d %02d:%02d:%02d",
+                                    "%04d-%02d-%02d %02d:%02d",
                                     year,
                                     monthNumber + 1,
                                     day,
                                     hour24,
-                                    min,
-                                    sec
+                                    min
                                 )
                             }
                         }).apply {
