cc43c9073daef4ea93fed8ffb060e2cb3175088c
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-12 19:36:26 +0900

    Add items in `OnBoardingAdapter`

--- app/src/main/java/kr/khs/oneboard/adapters/OnBoardingAdapter.kt
+++ app/src/main/java/kr/khs/oneboard/adapters/OnBoardingAdapter.kt
@@ -6,22 +6,58 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kr.khs.oneboard.R
 import kr.khs.oneboard.databinding.ViewOnboardingBinding
+import kr.khs.oneboard.utils.TYPE_PROFESSOR
+import kr.khs.oneboard.utils.UserInfoUtil
 
-class OnBoardingAdapter : RecyclerView.Adapter<OnBoardingAdapter.ViewHolder>() {
-    private val items = listOf(
-        R.drawable.onboarding_sample1,
-        R.drawable.onboarding_sample2,
-        R.drawable.onboarding_sample3
+class OnBoardingAdapter : RecyclerView.Adapter<OnBoardingAdapter.OnBoardingViewHolder>() {
+    private val items = arrayListOf(
+        R.drawable.onboarding_lecture_list,
+        R.drawable.onboarding_lecture_detail,
+        R.drawable.onboarding_navigation_menu,
+        R.drawable.onboarding_lecture_plan,
     )
 
-    class ViewHolder(private val binding: ViewOnboardingBinding) :
+    init {
+        if (UserInfoUtil.type == TYPE_PROFESSOR)
+            items.addAll(
+                arrayOf(
+                    R.drawable.onboarding_notice_list_professor,
+                    R.drawable.onboarding_notice_write
+                )
+            )
+        else
+            items.add(R.drawable.onboarding_notice_list_student)
+
+        if (UserInfoUtil.type == TYPE_PROFESSOR)
+            items.addAll(
+                arrayOf(
+                    R.drawable.onboarding_lesson_list_professor,
+                    R.drawable.onboarding_lesson_write
+                )
+            )
+        else
+            items.add(R.drawable.onboarding_lesson_list)
+
+        items.add(R.drawable.onboarding_lesson_detail)
+
+        if (UserInfoUtil.type == TYPE_PROFESSOR)
+            items.add(R.drawable.onboarding_attendance_professor)
+        else
+            items.add(R.drawable.onboarding_attendance_student)
+
+        if (UserInfoUtil.type == TYPE_PROFESSOR)
+            items.add(R.drawable.onboarding_grade_professor)
+        items.add(R.drawable.onboarding_grade_student)
+    }
+
+    class OnBoardingViewHolder(private val binding: ViewOnboardingBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(resId: Int) {
             Glide.with(binding.root).load(resId).into(binding.root)
         }
 
         companion object {
-            fun from(parent: ViewGroup): ViewHolder = ViewHolder(
+            fun from(parent: ViewGroup): OnBoardingViewHolder = OnBoardingViewHolder(
                 ViewOnboardingBinding.inflate(
                     LayoutInflater.from(parent.context),
                     parent,
@@ -31,11 +67,11 @@ class OnBoardingAdapter : RecyclerView.Adapter<OnBoardingAdapter.ViewHolder>() {
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder.from(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OnBoardingViewHolder {
+        return OnBoardingViewHolder.from(parent)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: OnBoardingViewHolder, position: Int) {
         holder.bind(items[position])
     }
 
