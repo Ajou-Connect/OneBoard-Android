026a7a5baa8146c68ed68a04def8f05091cb9cee
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-12 19:36:07 +0900

    Add `drawableRes` in `CreateonBoardingDialog()`

--- app/src/main/java/kr/khs/oneboard/utils/OnBoardingUtil.kt
+++ app/src/main/java/kr/khs/oneboard/utils/OnBoardingUtil.kt
@@ -23,14 +23,55 @@ fun Fragment.setOnBoardingSpf(fragmentName: String, value: Boolean = true) {
 private fun Fragment.getSPF(): SharedPreferences =
     requireContext().getSharedPreferences(SPF_NAME, Context.MODE_PRIVATE)
 
-fun Fragment.createOnBoardingDialog() {
+fun Fragment.createOnBoardingDialog(type: Boolean? = null) {
     val binding = DialogOnboardingBinding.inflate(layoutInflater)
 
     val builder = MaterialAlertDialogBuilder(requireContext())
         .setView(binding.root)
 
+    val drawableRes = when (this.javaClass.simpleName) {
+        "AnalysisFragment" -> R.drawable.onboarding_sample1
+        "AssignmentFragment" -> {
+            if (UserInfoUtil.type == TYPE_PROFESSOR)
+                R.drawable.onboarding_assignment_list_professor
+            else
+                R.drawable.onboarding_assignment_list_student
+        }
+        "AttendanceFragment" -> {
+            if (UserInfoUtil.type == TYPE_PROFESSOR)
+                R.drawable.onboarding_attendance_professor
+            else
+                R.drawable.onboarding_attendance_student
+        }
+        "GradeProfessorFragment" -> R.drawable.onboarding_grade_professor
+        "GradeStudentFragment" -> R.drawable.onboarding_grade_student
+        "LectureDetailFragment" -> R.drawable.onboarding_lecture_detail
+        "LecturePlanFragment" -> R.drawable.onboarding_lecture_plan
+        "LectureWriteFragment" -> {
+            if (type == TYPE_NOTICE)
+                R.drawable.onboarding_notice_write
+            else
+                R.drawable.onboarding_assignment_write
+        }
+        "LessonDetailFragment" -> R.drawable.onboarding_lesson_detail
+        "LessonListFragment" -> {
+            if (UserInfoUtil.type == TYPE_PROFESSOR)
+                R.drawable.onboarding_lesson_list_professor
+            else
+                R.drawable.onboarding_lesson_list
+        }
+        "LessonWriteFragment" -> R.drawable.onboarding_lesson_write
+        "NoticeFragment" -> {
+            if (UserInfoUtil.type == TYPE_PROFESSOR)
+                R.drawable.onboarding_notice_list_professor
+            else
+                R.drawable.onboarding_notice_list_student
+        }
+        "SubmitDetailFragment" -> R.drawable.onboarding_assignment_detail
+        else -> R.drawable.onboarding_sample1
+    }
     Glide.with(this)
-        .load(R.drawable.onboarding_sample1)
+        .load(drawableRes)
         .into(binding.dialogOnBoardingImageView)
 
     val dialog = builder.create()
