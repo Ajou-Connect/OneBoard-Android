5d413f4e1fee4840601e22aa2d84e8b310833f75
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-13 01:39:43 +0900

    Init `Analysis Fragment` (WIP : Current state is no test with server)

--- app/src/main/java/kr/khs/oneboard/ui/AnalysisFragment.kt
+++ app/src/main/java/kr/khs/oneboard/ui/AnalysisFragment.kt
@@ -1,10 +1,15 @@
 package kr.khs.oneboard.ui
 
 import android.graphics.Color
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import kr.khs.oneboard.R
 import kr.khs.oneboard.core.BaseFragment
 import kr.khs.oneboard.databinding.FragmentAnalysisBinding
 import kr.khs.oneboard.utils.DialogUtil
@@ -18,6 +23,44 @@ import kr.khs.oneboard.views.PieHelper
 class AnalysisFragment : BaseFragment<FragmentAnalysisBinding, AnalysisViewModel>() {
     override val viewModel: AnalysisViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.lessonList.observe(viewLifecycleOwner) { lessonList ->
+            initSpinner(lessonList.map { it.title })
+        }
+
+        viewModel.understandingList.observe(viewLifecycleOwner) { understandingList ->
+            val bottomTextList = ArrayList<String>()
+            val dataList = ArrayList<ArrayList<Int>>()
+            val oList = ArrayList<Int>()
+            val xList = ArrayList<Int>()
+
+            understandingList.forEach {
+                bottomTextList.add(it.createdDt)
+                oList.add(it.yes)
+                xList.add(it.no)
+            }
+            dataList.add(oList)
+            dataList.add(xList)
+
+            initPlot(
+                bottomTextList,
+                dataList
+            )
+        }
+
+        viewModel.quizList.observe(viewLifecycleOwner) { quizList ->
+            val correct = quizList[0].correctNum
+            val incorrect = quizList[0].incorrectNum
+
+            initPie(
+                (correct / (correct + incorrect.toFloat())) * 100,
+                (incorrect / (correct + incorrect.toFloat())) * 100
+            )
+        }
+    }
+
     override fun getFragmentViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -26,18 +69,36 @@ class AnalysisFragment : BaseFragment<FragmentAnalysisBinding, AnalysisViewModel
     override fun init() {
         binding.viewTitle.root.text = "분석 정보 확인"
 
-        initViews()
+        viewModel.getLessonList(parentViewModel.getLecture().id)
     }
 
-    private fun initViews() {
-        initPlot()
-        initPie()
+    private fun initSpinner(list: List<String>) {
+        with(binding.analysisSpinner) {
+            adapter = ArrayAdapter(requireContext(), R.layout.spinner_item, list)
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    viewModel.getAnalysisInfo(
+                        parentViewModel.getLecture().id,
+                        viewModel.lessonList.value!![position].lessonId
+                    )
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+
+                }
+            }
+        }
     }
 
-    private fun initPie() {
+    private fun initPie(oRatio: Float, xRatio: Float) {
         val pieHelperList = ArrayList<PieHelper>()
-        pieHelperList.add(PieHelper(80f, "O", Color.BLUE))
-        pieHelperList.add(PieHelper(20f, "X", Color.RED))
+        pieHelperList.add(PieHelper(oRatio, "O", Color.BLUE))
+        pieHelperList.add(PieHelper(xRatio, "X", Color.RED))
 
         with(binding.analysisPie) {
             setDate(pieHelperList)
@@ -52,28 +113,28 @@ class AnalysisFragment : BaseFragment<FragmentAnalysisBinding, AnalysisViewModel
         }
     }
 
-    private fun initPlot() {
-        val bottomTextList = ArrayList<String>()
-        val sampleDataList = ArrayList<ArrayList<Int>>()
-        val sample1 = ArrayList<Int>()
-        val sample2 = ArrayList<Int>()
-        val random = Math.random() * 9 + 1
-
-        for (i in 0 until 10) {
-            bottomTextList.add(i.toString())
-            sample1.add((Math.random() * random).toInt())
-            sample2.add((Math.random() * random).toInt())
-        }
-
-        sampleDataList.add(sample1)
-        sampleDataList.add(sample2)
+    private fun initPlot(bottomTextList: ArrayList<String>, dataList: ArrayList<ArrayList<Int>>) {
+//        val bottomTextList = ArrayList<String>()
+//        val dataList = ArrayList<ArrayList<Int>>()
+//        val sample1 = ArrayList<Int>()
+//        val sample2 = ArrayList<Int>()
+//        val random = Math.random() * 9 + 1
+//
+//        for (i in 0 until 10) {
+//            bottomTextList.add(i.toString())
+//            sample1.add((Math.random() * random).toInt())
+//            sample2.add((Math.random() * random).toInt())
+//        }
+//
+//        dataList.add(sample1)
+//        dataList.add(sample2)
 
         with(binding.analysisPlot) {
             setDrawDotLine(true)
             setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY)
             setBottomTextList(bottomTextList)
-            setColorArray(intArrayOf(Color.BLACK, Color.GREEN, Color.GRAY, Color.CYAN))
-            setDataList(sampleDataList)
+            setColorArray(intArrayOf(Color.RED, Color.BLUE, Color.GRAY, Color.CYAN))
+            setDataList(dataList)
         }
     }
 
