c1f2e45621e54d36f0fe66b692a346a7d3b4b87b
KHS-kr <ks96ks@ajou.ac.kr>
2021-12-19 21:52:05 +0900

    Remove `SDK KEY`, `SDK SECRET`

--- app/src/main/java/kr/khs/oneboard/utils/Credentials.kt
+++ app/src/main/java/kr/khs/oneboard/utils/Credentials.kt
@@ -2,5 +2,3 @@ package kr.khs.oneboard.utils
 
 const val WEB_DOMAIN = "https://zoom.us"
 const val token = ""
-const val sdkKey = "MoRylmD2jBq9NfbZXbSVmvZcGYOFkDCeJc3e"
-const val sdkSecret = "NewabYwGXIFrOlPRf4dZBKeqFECESIkdlLrq"
